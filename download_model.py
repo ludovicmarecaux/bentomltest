264b2acb28da649e309b123fb0a60b2e9006eda3
ludomare <108358050+ludovicmarecaux@users.noreply.github.com>
2023-10-12 23:50:28 +0200

    test avec HLN

--- download_model.py
+++ download_model.py
@@ -10,7 +10,7 @@ iris = datasets.load_iris()
 X, y = iris.data, iris.target
 
 # Train the model
-clf = SGDClassifier(loss="hinge", penalty="l2")
+clf = svm.SVC(kernel="linear")
 clf.fit(X, y)
 
 # Save model to the BentoML local Model Store
